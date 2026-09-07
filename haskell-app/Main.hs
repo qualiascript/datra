971407e7c80339a3cd7839a9ef3797b4287558a0
qualiascript <qualiascript@gmail.com>
2026-09-07 16:46:13 -0400

    haskell module cleanup

--- haskell-app/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main (main) where
-
-import DatraHaskell (datraHaskellFunc)
-
-main :: IO ()
-main = datraHaskellFunc
--- haskell-src/Datra.hs
+++
@@ -1,8 +0,0 @@
-module Datra (placeholder) where
-
--- | Placeholder exported by the root package until the implementation moves
--- here from the @datra-haskell@ package.
-main :: IO ()
-main = do
-  putStrLn "Hello, Datra from haskell-src!"
-  DatraHaskell.datraHaskellFunc
