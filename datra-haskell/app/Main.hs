443791cc5d12e700606c24397facbc94726c5847
qualiascript <qualiascript@gmail.com>
2026-09-08 14:49:47 -0400

    cleanup

--- datra-haskell/app/Main.hs
+++ datra-haskell/app/Main.hs
@@ -1,8 +1,5 @@
 module Main where
 
-import qualified DatraHaskell (datraHaskellFunc)
-
 main :: IO ()
 main = do
   putStrLn "Hello, Datra from main.hs!"
-  DatraHaskell.datraHaskellFunc
--- datra-haskell/src/DatraHaskell.hs
+++
@@ -1,4 +0,0 @@
-module DatraHaskell (datraHaskellFunc) where
-
-datraHaskellFunc :: IO ()
-datraHaskellFunc = putStrLn "Hello, Datra from DatraHaskell.hs!"
