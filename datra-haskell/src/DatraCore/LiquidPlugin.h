13ad2ea5768c22ee1123b2a6e482cec402816b13
qualiascript <qualiascript@gmail.com>
2026-09-11 20:18:30 -0400

    Revert "Reorganize model structure (#17)"
    This reverts commit 47be808a3000c5f5938bb4649fdc4ad42e115564.

--- datra-haskell/src/DatraCore/LiquidPlugin.h
+++ datra-haskell/src/DatraCore/LiquidPlugin.h
@@ -1,5 +1,3 @@
-#ifdef USE_LIQUIDHASKELL
 #ifndef __GHCIDE__
 {-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
-#endif
 #endif
--- datra-haskell/test/Main.hs
+++ datra-haskell/test/Main.hs
@@ -2,8 +2,18 @@
 
 module Main (main) where
 
+import Atlas
+import Chain
+import Consolidation
 import qualified Control.Category as Category
-import DataTransformations
+import DataTransformation
+import DatraOrdinal
+import DomanialInsertion
+import Dominion
+import FiniteDominion
+import Folio
+import PageElements
+import Pagination
 import Numeric.Natural (Natural)
 
 import Data.Maybe (isNothing)
@@ -71,8 +81,6 @@ testIdentityInsertion = do
       == map Just [0, 1, 2])
   assert "checked smart constructor applies forward"
     (applyInsertion checkedIdentity True)
-  assert "opposite insertion round-trips"
-    (applyInsertion (unop (op checkedIdentity)) True)
 
 testSpine :: IO ()
 testSpine = do
@@ -137,16 +145,16 @@ testConsolidation = do
     (map (applyConsolidation identity) values == values)
   assert "coconsolidation retains the underlying map"
     (map
-      (applyConsolidation (unop (op halve)))
+      (applyConsolidation (Consolidation.unop (Consolidation.op halve)))
       values
       == map (applyConsolidation halve) values)
   assert "coconsolidation composition follows opposite categorical order"
     (map
       (applyConsolidation
-        (unop
-          (composeCoconsolidations
-            (op halve)
-            (op halve))))
+        (Consolidation.unop
+          (Consolidation.composeCoconsolidations
+            (Consolidation.op halve)
+            (Consolidation.op halve))))
       values
       == map (applyConsolidation doubledHalve) values)
 
@@ -211,9 +219,9 @@ threePageFolio =
     (appendPage
       (singletonFolio unitChain)
       boolChain
-      (op collapseBool))
+      (Consolidation.op collapseBool))
     spine
-    (op nonzero)
+    (Consolidation.op nonzero)
 
 testConsolidationTransport :: IO ()
 testConsolidationTransport = do
@@ -243,8 +251,8 @@ testFolio =
       originUnique threePageFolio () `seq`
         folioMapIdentity False `seq`
           folioMapComposition
-            (op nonzero)
-            (op collapseBool)
+            (Consolidation.op nonzero)
+            (Consolidation.op collapseBool)
             7 `seq` pure ()
       assert "folio counts its genuine pages"
         (folioLength threePageFolio == 3)
