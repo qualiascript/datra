e014d06ea91afe7b6a200e88e9d430ff0bd2820d
qualiascript <qualiascript@gmail.com>
2026-09-14 16:40:21 -0400

    Coalitions (#22)
    * coalitions
    
    * domanial inclusion
    
    * reorg src

---
+++ datra-haskell/src/DatraCore/Atlases/Derived/Coalitions/Coalition/Coalition.hs
@@ -0,0 +1,15 @@
+-- | Coalitions and the Coalizing functor from stable Atlas transversals to
+-- domanial insertions.
+module Coalition
+  ( CoalitionElement
+  , coalitionElementRank
+  , withCoalitionElement
+  , coalition
+  , stableCoalitionMap
+  , coalizingFunctorObject
+  , coalizingFunctorHom
+  , coalizingFunctorIdentity
+  , coalizingFunctorComposition
+  ) where
+
+import Coalition.Internal
--- datra-haskell/test/Main.hs
+++ datra-haskell/test/Main.hs
@@ -10,11 +10,13 @@ import AtlasTransversal
 import AtlasTransversalMap
 import Chain
 import Charting
+import Coalition
 import Consolidation
 import qualified Control.Category as Category
 import DataTransformation
 import DataTransformationMap
 import DatraOrdinal
+import DomanialInclusion
 import DomanialInsertion
 import Dominion
 import Expedition
@@ -51,6 +53,8 @@ main = do
   testOrderedAtlasTransposal
   testAtlasTransversal
   testStableAtlasTransversal
+  testCoalition
+  testDomanialInclusion
 
 checkedIdentity :: DomanialInsertion Bool Bool
 checkedIdentity = domanialInsertion id Just (const ())
@@ -598,6 +602,19 @@ testAtlasMapData pageArrow =
         else Just
           (TestCellData
             (value - pageElementPage (arrowSource pageArrow))))
+    (const ())
+
+doubleTestCellData
+  :: DomanialInsertion
+       (TestCellData source)
+       (TestCellData target)
+doubleTestCellData =
+  domanialInsertion
+    (\(TestCellData value) -> TestCellData (value * 2))
+    (\(TestCellData value) ->
+      if even value
+        then Just (TestCellData (value `div` 2))
+        else Nothing)
     (const ())
 
 testAtlasIdentityLaw
@@ -1350,3 +1367,207 @@ testStableAtlasTransversal =
           stableAtlasTransversalPreservesExtent composed `seq`
             assert "stable Atlas transversals send extent to extent"
               (mappedOrigin == originElement)
+
+testCoalition :: IO ()
+testCoalition =
+  pagination (singletonFolio unitChain) $ \valuePagination ->
+    atlas
+      valuePagination
+      (atlasDataAction testAtlasDataAt testAtlasMapData)
+      testAtlasIdentityLaw
+      testAtlasCompositionLaw
+      testAtlasCoherenceLaw
+      testAtlasDisjointLaw $ \valueAtlas ->
+        let witness = atlasWitness valueAtlas
+            transposal =
+              atlasTransposal witness identityAtlasHom Just (const ())
+            ordered = orderedAtlasTransposal transposal (\_ _ -> ())
+            transversal =
+              atlasTransversal
+                valueAtlas
+                valueAtlas
+                ordered
+                (\_ targetOccurrence targetDatum ->
+                  atlasCoverageWitness
+                    valueAtlas
+                    targetOccurrence
+                    targetDatum
+                    targetOccurrence
+                    targetDatum
+                    ())
+            stable =
+              stableAtlasTransversal
+                valueAtlas valueAtlas transversal ()
+            doublingAction =
+              atlasMorphismAction
+                identityAtlasObjectMap
+                valueAtlas
+                valueAtlas
+                id
+                (const doubleTestCellData)
+                (\_ _ -> ())
+                (\_ _ -> ())
+            doublingTransposal =
+              atlasTransposal
+                witness
+                (atlasHom (atlasMorphism doublingAction))
+                Just
+                (const ())
+            doublingTransversal =
+              atlasTransversal
+                valueAtlas
+                valueAtlas
+                (orderedAtlasTransposal
+                  doublingTransposal (\_ _ -> ()))
+                (\_ targetOccurrence targetDatum ->
+                  atlasCoverageWitness
+                    valueAtlas
+                    targetOccurrence
+                    targetDatum
+                    targetOccurrence
+                    targetDatum
+                    ())
+            doublingStable =
+              stableAtlasTransversal
+                valueAtlas valueAtlas doublingTransversal ()
+            valueCoalition = coalition valueAtlas
+        in case
+          [ candidate
+          | index <- [0 .. 100]
+          , Just candidate <- [unrank valueCoalition index]
+          , coalitionElementRank candidate == 7
+          ] of
+            [] -> fail "Coalition lost a covered extent datum"
+            element : _ -> do
+              let decoded =
+                    withCoalitionElement element $ \origin datum ->
+                      ( pageElementPage origin
+                      , rank (atlasDataAt valueAtlas origin) datum
+                      )
+              assert "a coalition is the covered chart extent"
+                (decoded == (0, 7))
+              let mapped =
+                    applyInsertion
+                      (coalizingFunctorHom witness witness stable)
+                      element
+                  composed =
+                    coalizingFunctorHom
+                      witness
+                      witness
+                      (stable Category.. stable)
+              assert "the Coalizing functor maps the stable origin component"
+                (coalitionElementRank mapped == 7)
+              assert "the Coalizing arrow has the inherited partial inverse"
+                (preimage
+                  (coalizingFunctorHom witness witness stable)
+                  mapped == Just element)
+              assert "a coalition preimage outside the arrow image is total"
+                (preimage
+                  (coalizingFunctorHom
+                    witness witness doublingStable)
+                  element == Nothing)
+              assert "the coalition dominion rank round-trips"
+                (unrank valueCoalition (rank valueCoalition element)
+                  == Just element)
+              coalizingFunctorIdentity witness element `seq`
+                coalizingFunctorComposition
+                  witness witness witness stable stable element `seq`
+                    assert "the Coalizing functor preserves composition"
+                      (coalitionElementRank
+                        (applyInsertion composed element) == 7)
+
+testDomanialInclusion :: IO ()
+testDomanialInclusion = do
+  let naturals = dominion id Just (const ())
+      successor :: Natural -> Natural
+      successor = (+ 1)
+      twice :: Natural -> Natural
+      twice = (* 2)
+      included = dominionAtlas naturals
+      includedWitness = atlasWitness included
+      (fromCoalition, intoCoalition) = coaDomIncIso naturals
+      seven = applyInsertion intoCoalition 7
+
+  assert "Coa (DomInc X) is isomorphic to X"
+    (applyInsertion fromCoalition seven == 7)
+  assert "the Coa-DomInc counit has a total inverse"
+    (preimage fromCoalition 7 == Just seven)
+  assert "the Coa-DomInc unit has a total inverse"
+    (preimage intoCoalition seven == Just 7)
+
+  coaDomIncIsoLeftInverse naturals seven `seq`
+    coaDomIncIsoRightInverse naturals 7 `seq` pure ()
+
+  let includedIdentity =
+        dominionMap naturals naturals
+          (identityInsertion :: DomanialInsertion Natural Natural)
+      adjointArrow =
+        domIncCoaHomEquivTo naturals includedWitness includedIdentity
+      restoredArrow =
+        domIncCoaHomEquivFrom naturals includedWitness adjointArrow
+      mappedSeven = applyInsertion adjointArrow 7
+
+  assert "the forward adjunction map lands in the target coalition"
+    (applyInsertion fromCoalition mappedSeven == 7)
+  assert "the forward adjunction map retains its inverse witness"
+    (preimage adjointArrow mappedSeven == Just 7)
+
+  withPageElement (atlasOriginCell included) $ \origin ->
+    case unrank (atlasDataAt included origin) 7 of
+      Nothing -> fail "Domanial Inclusion lost its constant cell datum"
+      Just sourceDatum ->
+        withAtlasMorphismImage
+          (mapStableAtlasTransversalData
+            includedWitness restoredArrow origin) $ \_ component -> do
+              let restoredDatum = applyInsertion component sourceDatum
+              assert "the two adjunction maps are inverse on data"
+                (dominionCellDataValue restoredDatum == 7)
+              assert "the reconstructed component inverse is total on images"
+                (preimage component restoredDatum == Just sourceDatum)
+
+              let observe datum = dominionCellDataValue datum + 1
+              domIncCoaHomEquivLeftInverse observe sourceDatum `seq`
+                domIncCoaHomEquivRightInverse successor 7 `seq`
+                  domIncCoaHomEquivNaturalityLeft
+                    successor observe 7 `seq`
+                      domIncCoaHomEquivNaturalityRight
+                        observe twice 7 `seq` pure ()
+
+  let double =
+        domanialInsertion
+          twice
+          (\value ->
+            if even value then Just (value `div` 2) else Nothing)
+          (const ())
+      increment =
+        domanialInsertion
+          successor
+          (\value ->
+            if value == 0 then Nothing else Just (value - 1))
+          (const ())
+      direct = dominionMap naturals naturals
+        (composeInsertions increment double)
+      staged =
+        composeStableAtlasTransversals
+          (dominionMap naturals naturals increment)
+          (dominionMap naturals naturals double)
+
+  withPageElement (atlasOriginCell included) $ \origin ->
+    case unrank (atlasDataAt included origin) 4 of
+      Nothing -> fail "Domanial Inclusion failed to decode a source datum"
+      Just sourceDatum ->
+        withAtlasMorphismImage
+          (mapStableAtlasTransversalData includedWitness direct origin) $
+            \_ directComponent ->
+              withAtlasMorphismImage
+                (mapStableAtlasTransversalData includedWitness staged origin) $
+                  \_ stagedComponent -> do
+                    assert "the Domanial Inclusion preserves composition"
+                      ( dominionCellDataValue
+                          (applyInsertion directComponent sourceDatum)
+                        == dominionCellDataValue
+                          (applyInsertion stagedComponent sourceDatum)
+                      )
+                    domanialInclusionFunctorIdentity sourceDatum `seq`
+                      domanialInclusionFunctorComposition
+                        twice successor sourceDatum `seq` pure ()
